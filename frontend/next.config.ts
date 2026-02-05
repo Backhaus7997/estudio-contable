7d1f715de29afdd86357fa082733def062bdfc0c
Backhaus <tinchoignacio33@gmail.com>
2026-02-05 15:44:20 -0300

    pruebas para deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,5 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 };
+/** @type {import('next').NextConfig} */
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const isProd = process.env.NODE_ENV === 'production';
 
+module.exports = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  images: { unoptimized: true },
+};
 export default nextConfig;
